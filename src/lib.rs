edefe0a5531fada793c6abcc196f7eecacfc9736
Evgeny Kuzyakov <ek@nearprotocol.com>
2022-02-01 11:16:44 -0800

    Parsing Token and NFT metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,9 @@
-mod util;
 mod web4;
 
-pub use account::*;
-pub use article::*;
-use util::*;
-
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
-use near_sdk::json_types::ValidAccountId;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, near_bindgen, AccountId, Balance, BorshStorageKey, PanicOnDefault};
-
-near_sdk::setup_alloc!();
-
-#[derive(BorshSerialize, BorshStorageKey)]
-enum StorageKey {
-    Accounts,
-    AccountArticles { account_id: AccountId },
-    Articles,
-    ArticleIds,
-}
+use near_sdk::{env, near_bindgen, AccountId};
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-pub struct Contract {
-    accounts: UnorderedMap<AccountId, VAccount>,
-    articles: LookupMap<ArticleId, VArticle>,
-    article_ids: UnorderedSet<ArticleId>,
-}
-
-#[near_bindgen]
-impl Contract {
-    #[init]
-    pub fn new() -> Self {
-        Self {
-            accounts: UnorderedMap::new(StorageKey::Accounts),
-            articles: LookupMap::new(StorageKey::Articles),
-            article_ids: UnorderedSet::new(StorageKey::ArticleIds),
-        }
-    }
-
-    #[payable]
-    pub fn donate(&mut self) {
-        let account_id = env::predecessor_account_id();
-        let mut account = self.internal_get_account_or_default(&account_id);
-        account.near_deposit += env::attached_deposit();
-        self.internal_set_account(&account_id, account);
-    }
-}
+#[derive(BorshDeserialize, BorshSerialize, Default)]
+pub struct Contract {}
--- src/web4.rs
+++ src/web4.rs
@@ -1,5 +1,10 @@
 use crate::*;
+use near_contract_standards::non_fungible_token::metadata::NFTContractMetadata;
+use near_contract_standards::non_fungible_token::Token;
+use near_sdk::json_types::Base64VecU8;
+use near_sdk::serde_json;
 use std::collections::HashMap;
+use std::str::FromStr;
 
 const INDEX_BODY: &str = include_str!("../res/index.html");
 
@@ -8,8 +13,8 @@ const INDEX_BODY: &str = include_str!("../res/index.html");
 #[serde(crate = "near_sdk::serde")]
 pub struct Web4Request {
     #[serde(rename = "accountId")]
-    account_id: Option<ValidAccountId>,
-    path: Option<String>,
+    account_id: Option<AccountId>,
+    path: String,
     params: Option<HashMap<String, String>>,
     query: Option<HashMap<String, Vec<String>>>,
     preloads: Option<HashMap<String, Web4Response>>,
@@ -21,7 +26,7 @@ pub struct Web4Response {
     #[serde(rename = "contentType")]
     content_type: Option<String>,
     status: Option<u32>,
-    body: Option<Vec<u8>>,
+    body: Option<Base64VecU8>,
     #[serde(rename = "bodyUrl")]
     body_url: Option<String>,
     #[serde(rename = "preloadUrls")]
@@ -32,7 +37,7 @@ impl Web4Response {
     pub fn html_response(text: String) -> Self {
         Self {
             content_type: Some(String::from("text/html; charset=UTF-8")),
-            body: Some(text.into_bytes()),
+            body: Some(text.into_bytes().into()),
             ..Default::default()
         }
     }
@@ -40,7 +45,7 @@ impl Web4Response {
     pub fn plain_response(text: String) -> Self {
         Self {
             content_type: Some(String::from("text/plain; charset=UTF-8")),
-            body: Some(text.into_bytes()),
+            body: Some(text.into_bytes().into()),
             ..Default::default()
         }
     }
@@ -67,8 +72,6 @@ impl Web4Response {
     }
 }
 
-const PREFIX_HISTORY: &str = "/history/";
-
 fn filter_string(s: String) -> String {
     s.chars()
         .into_iter()
@@ -86,41 +89,78 @@ fn filter_string(s: String) -> String {
 impl Contract {
     #[allow(unused_variables)]
     pub fn web4_get(&self, request: Web4Request) -> Web4Response {
-        let path = request.path.expect("Path expected");
-        if path.starts_with("/static/") || path == "/favicon.png" || path == "/manifest.json" {
-            return Web4Response::body_url(
-                String::from("ipfs://bafybeigifbsj3nnbufxa3non7xas23r3yqjlfx3v3k27qgdgch2mmqdeue")
-                    + &path,
-            );
-        }
+        let path = request.path;
+        let (nft_account_id, token_id) = path[1..].split_once('/').expect("Token ID is missing");
+        let nft_account_id = AccountId::from_str(nft_account_id).expect("Invalid NFT account ID");
+        let nft_metadata_url =
+            format!("/web4/contract/{}/nft_metadata", nft_account_id.to_string());
+        let token_url = format!(
+            "/web4/contract/{}/nft_token?token_id={}",
+            nft_account_id.to_string(),
+            token_id
+        );
 
-        if path == "/robots.txt" {
-            return Web4Response::plain_response("User-agent: *\nDisallow:".to_string());
-        }
-
-        let article_id = path
-            .rfind('/')
-            .map(|p| path[(p + 1)..].to_string())
-            .unwrap_or_default();
-
-        let escaped_article_id = filter_string(article_id.clone());
-
-        let title = if path.starts_with(PREFIX_HISTORY) {
-            format!("Edit history of {} | wiki", escaped_article_id)
+        if let Some(preloads) = request.preloads {
+            let token: Token = serde_json::from_slice(
+                &preloads
+                    .get(&token_url)
+                    .unwrap()
+                    .body
+                    .as_ref()
+                    .expect("Token not found")
+                    .0,
+            )
+            .expect("Failed to parse token");
+            let nft_metadata: NFTContractMetadata = serde_json::from_slice(
+                &preloads
+                    .get(&nft_metadata_url)
+                    .unwrap()
+                    .body
+                    .as_ref()
+                    .expect("NFT Metadata doesn't exist")
+                    .0,
+            )
+            .expect("Failed to parse NFT Metadata");
+            Web4Response::plain_response(serde_json::to_string(&token).unwrap())
         } else {
-            format!("{} | wiki", escaped_article_id)
-        };
-
-        let article = self.internal_get_article(&article_id);
-
-        let description = article
-            .map(|article| filter_string(article.body))
-            .unwrap_or_else(|| "the wiki built on NEAR".to_string());
-
-        Web4Response::html_response(
-            INDEX_BODY
-                .replace("The wiki", &title)
-                .replace("the wiki built on NEAR", &description),
-        )
+            Web4Response::preload_urls(vec![nft_metadata_url, token_url])
+        }
+        //
+        // let path = request.path.expect("Path expected");
+        // if path.starts_with("/static/") || path == "/favicon.png" || path == "/manifest.json" {
+        //     return Web4Response::body_url(
+        //         String::from("ipfs://bafybeigifbsj3nnbufxa3non7xas23r3yqjlfx3v3k27qgdgch2mmqdeue")
+        //             + &path,
+        //     );
+        // }
+        //
+        // if path == "/robots.txt" {
+        //     return Web4Response::plain_response("User-agent: *\nDisallow:".to_string());
+        // }
+        //
+        // let article_id = path
+        //     .rfind('/')
+        //     .map(|p| path[(p + 1)..].to_string())
+        //     .unwrap_or_default();
+        //
+        // let escaped_article_id = filter_string(article_id.clone());
+        //
+        // let title = if path.starts_with(PREFIX_HISTORY) {
+        //     format!("Edit history of {} | wiki", escaped_article_id)
+        // } else {
+        //     format!("{} | wiki", escaped_article_id)
+        // };
+        //
+        // let article = self.internal_get_article(&article_id);
+        //
+        // let description = article
+        //     .map(|article| filter_string(article.body))
+        //     .unwrap_or_else(|| "the wiki built on NEAR".to_string());
+        //
+        // Web4Response::html_response(
+        //     INDEX_BODY
+        //         .replace("The wiki", &title)
+        //         .replace("the wiki built on NEAR", &description),
+        // )
     }
 }
